8be860ad5223dc975cbe23caf1324cea87913c14
ozarkcreekgauges <82779269+ozarkcreekgauges@users.noreply.github.com>
2021-04-19 12:01:32 -0500

    Update ocg.py

--- ocg.py
+++ ocg.py
@@ -42,14 +42,28 @@ def writeToFile(form):
         "Location: "+ form['location2'] + "\n" +
         "Message: "+ form['message'] + "\n"
 
-    msg = EmailMessage()
-    msg.set_content(message)
-    msg['Subject'] = form['rivername'] + "addition"
-    msg['From'] = "ozarkcreekgauges@gmail.com"
-    msg['To'] = "ozarkcreekgauges@gmail.com"
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = "ozarkcreekgauges@gmail.com"
+    password = "WeLoveBoats1!"
+      
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+      
+    # Try to log in to server and send email
+    try:
+          server = smtplib.SMTP(smtp_server,port)
+          server.ehlo() # Can be omitted
+          server.starttls(context=context) # Secure the connection
+          server.ehlo() # Can be omitted
+          server.login(sender_email, password)
+          #Send email here
+          server.sendmail("ozarkcreekgauges@gmail.com", "ozarkcreekgauges@gmail.com", message)
+    except Exception as e:
+          # Print any error messages to stdout
+          print(e)
+    finally:
+          server.quit() 
 
 #Main/Table page
 @app.route('/',methods=["GET"])
